9677f179a0704b81d879f591de01b81b802a49af
jetwong.wang <jetwong.wang@happyelements.com>
2022-05-16 14:45:37 +0800

    修改

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,125 +1,100 @@
 <template>
   <div style="height: 100%">
     <el-container>
-      <!-- 页面左部菜单+logo -->
-      <el-aside class="aside">
-        <div class="left-top">
-          <div class="logo-mobile" v-if="isCollapse" v-bind:style="{ width: sideBarWidth, transition: transformChange }">
-            <img src="@/assets/img/mobile-logo.png" alt="" />
-          </div>
-          <div class="logo" v-else v-bind:style="{ width: sideBarWidth, transition: transformChange }">
+      <!-- 页面头部 -->
+      <el-header class="header" ref="header">
+        <div class="header-logo">
+          <div class="logo">
             <img src="@/assets/img/logo.png" alt="" />
           </div>
         </div>
-        <div class="sidebar-menu">
+        <div class="header-content">
+          <div class="left">
+            <div class="app-box">
+              <Icon name="contract" scale="2.7"></Icon>
+              <p>合同</p>
+            </div>
+            <div class="app-box">
+              <Icon name="loan" scale="2.7"></Icon>
+              <p>请款</p>
+            </div>
+            <div class="app-box">
+              <Icon name="exp" scale="2.7"></Icon>
+              <p>报销</p>
+            </div>
+            <div class="app-box">
+              <Icon name="purchase" scale="2.7"></Icon>
+              <p>采购</p>
+            </div>
+            <div class="app-box">
+              <Icon name="set" scale="2.7"></Icon>
+              <p>系统设置</p>
+            </div>
+          </div>
+          <div class="right">
+            <el-input placeholder="请输入内容" prefix-icon="el-icon-search" v-model="input2" class="search-box"></el-input>
+            <Icon name="message" scale="4.5" class="message-tips"></Icon>
+            <div><img class="nav-avatar" src="http://uc.happyelements.net/admin/data2/small-pic/jetwong.wang.jpg" alt="头像" /></div>
+            <div class="nickname-box">
+              <el-dropdown class="dropdownBtn" @command="accountAction">
+                <el-button>张三丰<i class="el-icon-arrow-down el-icon--right"></i> </el-button>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item command="updateCache">更新缓存</el-dropdown-item>
+                  <el-dropdown-item command="logout">退出登录</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </div>
+          </div>
+        </div>
+      </el-header>
+      <!-- 页面左部菜单 -->
+      <el-container>
+        <el-aside class="aside" ref="aside">
           <div style="height: 100%">
             <side-bar :isCollapse="isCollapse"></side-bar>
           </div>
-        </div>
-      </el-aside>
-      <!-- 页面头部+主体内容部分 -->
-      <el-container>
-        <el-header class="header" v-bind:style="{ marginLeft: mainMarginLeft, transition: transformChange }">
-          <div class="left">
-            <div class="hide-sidebar" v-bind:style="{ transition: transformChange }">
-              <Icon v-if="isCollapse" name="out" width="20" height="20" class="title-icon" @click.native="changeSlidebarState"></Icon>
-              <Icon v-else name="collapse" width="20" height="20" class="title-icon" @click.native="changeSlidebarState"></Icon>
-            </div>
-          </div>
-          <div class="center">
-            <div v-if="!isCollapse" class="sidebar-title">{{ title }}</div>
-          </div>
-          <div class="right">
-            <nav-bar></nav-bar>
-          </div>
-        </el-header>
-        <el-main ref="main" v-bind:style="{ marginLeft: mainMarginLeft, transition: transformChange }">
+        </el-aside>
+        <!-- 页面主体 -->
+        <el-main ref="main">
           <div class="main-container">
             <app-main ref="appMain"></app-main>
             <div class="copyright">{{ copyright }}</div>
           </div>
         </el-main>
       </el-container>
-      <div class="sidenav-mask" @click="changeSlidebarState"></div>
     </el-container>
   </div>
 </template>
 
 <script>
-import { NavBar, SideBar, AppMain } from '@/components/layout'
+import { SideBar, AppMain } from '@/components/layout'
 
-const navBarHeight = 60 // header高度
+const navBarHeight = 70 // header高度
 const copyrightHeight = 48 // 历史记录栏与舞台的间距
-const sideBarWidth = '210px'
-const collapseSideBarWidth = '64px'
-const totalHeight = navBarHeight + copyrightHeight + 60 // 总高度=头部高度+历史栏记录高度+两者间距
+const totalHeight = navBarHeight + copyrightHeight + 70 // 总高度=头部高度+历史栏记录高度+两者间距
 
 export default {
   data() {
     return {
       isCollapse: false, // 左侧菜单栏是否折叠
-      sideBarWidth, // 左侧菜单栏展开的宽度
-      clientWidth: 0, // 页面宽度
       clientHeight: 0, // 页面高度
-      transformChange: '',
-      sideBarMargin: 'auto auto',
-      mainMarginLeft: '0px',
       title: window.SYSTEM_TITLE,
       copyright: window.COPYRIGHT
     }
   },
   mounted() {
     this.setResize()
-    // 宽度768为界限，区分左侧菜单是否折叠
-    if (this.clientWidth <= 768) {
-      this.isCollapse = true
-    } else {
-      this.isCollapse = false
-    }
-    // 监测屏幕宽度 折叠左侧菜单栏
-    window.onresize = () => {
-      const that = this
-      that.setResize()
-      if (that.clientWidth <= 768) {
-        if (that.isCollapse === false) {
-          that.isCollapse = true
-        }
-      } else if (that.isCollapse === true) {
-        that.isCollapse = false
-      }
-    }
   },
   inject: ['eventBus'],
   methods: {
-    // 控制菜单折叠
-    changeSlidebarState() {
-      this.isCollapse = !this.isCollapse
-      if (this.isCollapse) {
-        this.sideBarWidth = collapseSideBarWidth
-        this.transformChange = 'all 0.4s'
-        this.sideBarMargin = 'auto 16px auto auto'
-        this.mainMarginLeft = '-147px'
-      } else {
-        this.transformChange = 'all 0.4s'
-        this.sideBarMargin = 'auto auto'
-        this.mainMarginLeft = '0px'
-      }
-    },
     // 响应页面的宽度高度变化
     setResize() {
       this.clientHeight = document.body.clientHeight
-      this.clientWidth = document.body.clientWidth
       this.$refs.appMain.$el.style.minHeight = `${this.clientHeight - totalHeight + 30}px` // 计算主体内容最小高度
     }
   },
-  watch: {
-    isCollapse() {
-      this.transX = 0
-      this.sideBarWidth = this.isCollapse === false ? sideBarWidth : collapseSideBarWidth
-    }
-  },
+  watch: {},
   components: {
-    NavBar,
     SideBar,
     AppMain
   }
@@ -127,115 +102,113 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.left-top {
-  position: absolute;
-  z-index: 1000;
-  // border-bottom: 2px solid #585757;
-  background: #ebf1f9;
-  // background: -webkit-linear-gradient(left, #ffffe4, #dbdfff);
-}
-.logo {
-  width: $sidebar-width;
-  height: 100px;
-  position: relative;
-
-  img {
-    width: auto;
-    position: absolute;
-    left: 0px;
-    right: 0px;
-    top: 0px;
-    margin: auto;
-    bottom: 0px;
-  }
-}
-.logo-mobile {
-  width: $collapse-sidebar-width;
-  height: 100px;
-  position: relative;
-
-  img {
-    width: auto;
-    position: absolute;
-    left: 0px;
-    right: 0px;
-    top: 0px;
-    margin: auto;
-    bottom: 0px;
-  }
-}
-.sidebar-title {
-  width: 81%;
-  display: inline-block;
-  height: 60px;
-  line-height: 60px;
-  font-weight: bold;
-  color: white;
-  letter-spacing: 2px;
-  text-align: center;
-}
-.sidebar-menu {
-  height: 100%;
-  padding-top: 100px;
-  width: $sidebar-width;
-}
-.main-container {
-  margin: 16px 16px 0px 16px;
-}
-.hide-sidebar {
-  width: 100%;
-  height: 60px;
-  position: relative;
-  color: #797979;
-}
-.copyright {
-  display: block;
-  align-items: center;
-  justify-content: center;
-  line-height: 48px;
-  color: #797979;
-  font-size: 13px;
-  text-align: center;
-}
-.aside {
-  background: $sidebar-background;
-  overflow-x: hidden;
-  overflow-y: hidden;
-  width: $sidebar-width !important;
-  border: 0px !important;
-
-  &::-webkit-scrollbar {
-    width: 0px;
-    height: 0px;
-  }
-}
-
 .header {
   padding: 0;
   background: $header-background;
   background: -webkit-linear-gradient(left, $header-start-background, $header-end-background);
   height: $header-height !important;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
   box-shadow: 0px 2px 6px 0px rgba(190, 204, 216, 0.4);
+  display: flex;
 
-  .left {
-    flex-grow: 1;
-    z-index: 1000;
-    height: 60px;
+  .header-logo {
+    width: 210px;
+
+    .logo {
+      width: $sidebar-width;
+      height: $header-height;
+      position: relative;
+
+      img {
+        width: auto;
+        position: absolute;
+        left: 0px;
+        right: 0px;
+        top: 0px;
+        margin: auto;
+        bottom: 0px;
+      }
+    }
   }
 
-  .center {
-    flex-grow: 2;
-    color: white;
-    font-size: 27px;
-    font-weight: bolder;
-  }
+  .header-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: calc(100vw - 210px);
+    .left {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-left: 10px;
 
-  .right {
-    flex-grow: 1;
-    float: right;
+      .app-box {
+        width: 80px;
+        margin-right: 40px;
+        text-align: center;
+      }
+
+      p {
+        color: white;
+        font-size: 14px;
+        font-weight: bold;
+        letter-spacing: 1px;
+        margin-top: 4px;
+      }
+    }
+
+    .center {
+      flex-grow: 2;
+      color: white;
+      font-size: 27px;
+      font-weight: bolder;
+    }
+
+    .right {
+      display: flex;
+      align-items: center;
+
+      .nickname-box {
+        margin-right: 25px;
+
+        /deep/ .el-button {
+          background: unset;
+          border: 0px;
+          color: white;
+          font-size: 14px;
+        }
+      }
+      .message-tips {
+        margin-right: 30px;
+      }
+
+      .search-box {
+        margin-right: 50px;
+      }
+
+      .nav-avatar {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+      }
+
+      /deep/ .el-input__inner {
+        width: 300px;
+        border-radius: 8px;
+      }
+    }
+  }
+}
+.aside {
+  overflow-x: hidden;
+  overflow-y: hidden;
+  width: $sidebar-width !important;
+  border: 0px !important;
+  padding-top: 18px;
+  background: $sidebar-background;
+
+  &::-webkit-scrollbar {
+    width: 0px;
+    height: 0px;
   }
 }
 
@@ -244,28 +217,19 @@ export default {
   position: relative;
   padding: 0;
   // border-left: 1px solid #D5D1D1;
-}
 
-.sidenav-mask {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.3);
-  z-index: 10;
-  display: none;
-  cursor: pointer;
-
-  &.show {
-    display: block;
+  .main-container {
+    margin: 16px 16px 0px 16px;
   }
-}
-.title-icon {
-  width: 20px;
-  height: 20px;
-  color: white;
-  margin: 20px 15px;
-  cursor: pointer;
+
+  .copyright {
+    display: block;
+    align-items: center;
+    justify-content: center;
+    line-height: 48px;
+    color: #797979;
+    font-size: 13px;
+    text-align: center;
+  }
 }
 </style>
